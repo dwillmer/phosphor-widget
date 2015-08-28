94d165705d3fc0826c41666a649fc68a814fa29b
S. Chris Colbert <sccolbert@gmail.com>
2015-08-28 10:25:25 -0400

    ws commit

--- test/karma-cov.conf.js
+++ test/karma-cov.conf.js
@@ -4,16 +4,13 @@ module.exports = function (config) {
     frameworks: ['browserify', 'mocha'],
     reporters: ['mocha', 'coverage'],
     preprocessors: { 'build/index.js': ['browserify'] },
-    browserify: {
-        debug: true,
-        transform: ['browserify-istanbul']
-    },
+    browserify: { debug: true, transform: ['browserify-istanbul'] },
     files: ['build/index.js'],
     coverageReporter: {
-        reporters : [
-            {"type": "text"},
-            {"type": "html", dir: 'coverage'}
-        ]
+      reporters : [
+        { 'type': 'text' },
+        { 'type': 'html', dir: 'coverage' }
+      ]
     },
     port: 9876,
     colors: true,
