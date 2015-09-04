eb9af7db5b2d03a03f88e442f3958d027c4ca94d
S. Chris Colbert <sccolbert@gmail.com>
2015-09-04 00:42:32 -0400

    update to TS 1.6

--- scripts/dtsbundle.js
+++
@@ -1,7 +0,0 @@
-require('dts-generator').generate({
-  name: 'phosphor-widget',
-  main: 'phosphor-widget/index',
-  baseDir: 'lib',
-  files: ['index.d.ts'],
-  out: 'lib/phosphor-widget.d.ts',
-});
